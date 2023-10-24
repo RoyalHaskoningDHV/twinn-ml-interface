c373ed6258bd13e0b8b2214507a395581b8f621a
Steffen Buergers <steffen.burgers@rhdhv.com>
2023-10-24 12:58:01 +0200

    add ModelException class

--- twinn_ml_interface/objectmodels/Exceptions.py
+++ twinn_ml_interface/objectmodels/Exceptions.py
@@ -32,3 +32,46 @@ class ErrorMessages:
 class BaseError(Protocol):
     INTERNAL_MESSAGE: str = None
     CLIENT_MESSAGE: str = None
+
+
+def construct_model_exception_text(internaL_message: str, client_message: str) -> str:
+    """Build a parsable log string
+
+    Parameters
+    ----------
+    internaL_message : str
+        The internal message to add to the log
+    client_message : str
+        The client message to add to the log
+
+    Returns
+    -------
+    str
+        A parsable log string containing the internal message and client message
+    """
+    return (
+        f"{MessageType.INTERNAL_MESSAGE_TEXT.value}: {internaL_message}\n"
+        f"{MessageType.CLIENT_MESSAGE_TEXT.value}: {client_message}"
+    )
+
+
+class ModelException(Exception):
+    """
+    Custom Exception that allows the use of BaseError
+    """
+
+    def __init__(self, error_code: BaseError):
+        """
+        Parameters
+        ----------
+        error_code : BaseError
+            The desired error we want to raise
+        """
+        self.error_code = error_code
+        super().__init__(self.error_code.INTERNAL_MESSAGE)
+
+    def __str__(self):
+        msg = construct_model_exception_text(
+            self.error_code.INTERNAL_MESSAGE, self.error_code.CLIENT_MESSAGE
+        )
+        return msg
