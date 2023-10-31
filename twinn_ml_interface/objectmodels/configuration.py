d809837dabab2758dd70068ca2969d596e442a16
mi2354 <37376324+mi2354@users.noreply.github.com>
2023-10-31 11:45:28 +0100

    Configuration api (#3)
    * Rename files, add configuration class
    
    * Change type hint in Configuration
    
    ---------
    
    Co-authored-by: Jesse de Ruijter <jesse.de.ruijter@rhdhv.com>

---
+++ twinn_ml_interface/objectmodels/configuration.py
@@ -0,0 +1,32 @@
+from functools import cached_property
+from typing import Any, Protocol
+
+from .hierarchy_classes import TagType
+
+
+class Configuration(Protocol):
+    @cached_property
+    def target_name(self) -> str:
+        """Retrieve the target of the model in format UNIT_CODE:TAG_NAME.
+
+        The InputData will have a corresponding column with this name."""
+        ...
+
+    @property
+    def tenant(self) -> dict[str, Any]:
+        ...
+
+    @cached_property
+    def unit_properties(self) -> dict[TagType, dict[str, Any]]:
+        """Get and cache the ."""
+        ...
+
+    @cached_property
+    def unit_hierarchies(self) -> dict[str, list[str]]:
+        """Get and cache the ."""
+        ...
+
+    @cached_property
+    def tenant_config(self) -> dict[str, Any]:
+        """Get and cache the tenant_config."""
+        ...
