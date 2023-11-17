257a94204e60142e0888c7530138c1e25d865788
Miguel Hernandez <miguel.hernandez@rhdhv.com>
2023-11-17 12:44:08 +0100

    db_logs added

--- twinn_ml_interface/objectmodels/logging.py
+++ twinn_ml_interface/objectmodels/logging.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 from datetime import datetime, timezone
-from os import PathLike
-from typing import Any
+from os import PathLike, listdir
+from os.path import isdir, isfile
+from typing import Hashable
 
 
 @dataclass
@@ -32,8 +33,9 @@ class MetaDataLogger:
     """
 
     metrics: list[Metric]
-    params: dict[str, Any]
+    params: dict[str, Hashable]
     artifacts: list[dict[str | PathLike, str | None]]
+    db_logs: dict[str, Hashable]
 
     def __init__(self):
         self.reset_cache()
@@ -56,13 +58,23 @@ class MetaDataLogger:
         """
         self.metrics += metrics
 
-    def log_params(self, params: dict):
+    def log_params(self, params: dict[str, Hashable]):
         """Log multiple parameters. If parameter was logged in same run before, it is overwritten.
 
         Args:
-            params (dict): Each dictionary entry denotes one parameter description: value pair.
+            params (dict[str, Hashable]): Each dictionary entry denotes one parameter
+                as key value pair.
         """
         self.params |= params
+
+    def log_db_logs(self, db_log: dict[str, Hashable]):
+        """Log special elements that will be stored in a DB (access by API). If the element was
+        logged in the same run before, it is overwritten.
+
+        Args:
+            params (dict[str, Hashable]): Each dictionary entry denotes one lo as key value pair.
+        """
+        self.db_logs |= db_log
 
     def log_artifacts_in_dir(self, local_dir: PathLike, label: str | None = None):
         """Log an artifact / file.
@@ -82,9 +94,58 @@ class MetaDataLogger:
         """
         self.artifacts.extend(artifacts)
 
+    def is_metric_in_logger(self, metric_name: str, metric_value: float | None = None) -> bool:
+        """Check is a certain metric is inside the logger
+
+        Args:
+            metric_name (str): the metric to check
+            metric_value (float | None, optional): The value of the metric, will be checked only
+                if provided. Defaults to None.
+
+        Returns:
+            bool: wether if the metric is inside the logger with a certain value (optional)
+        """
+        for element in self.metrics:
+            if metric_name == element.key and (
+                metric_value is None or metric_value == element.value
+            ):
+                return True
+        return False
+
+    def get_metric_value_from_logger(self, metric_name: str) -> float | None:
+        """Get the metric value from the logger if it exists, else None
+
+        Args:
+            metric_name (str): the metric to check
+
+        Returns:
+            float | None: MEtrics value if exists, else None
+        """
+        for element in self.metrics:
+            if metric_name == element.key:
+                return element.value
+        return None
+
+    def get_artifact_names_from_logger(self) -> set[str]:
+        """Get get all artifact names that have been stored in the logger
+
+        Returns:
+            set[str]: a set with the names of all the artifacts stored
+        """
+        artifact_names = set()
+        for path_dict in self.artifacts:
+            path = list(path_dict)[0]
+            if isfile(path):
+                artifact_names.add(path.split("/")[-1].split(".")[0])
+            elif isdir(path):
+                for image_filename in listdir(path):
+                    artifact_names.add(image_filename.split(".")[0])
+        return artifact_names
+
     def reset_cache(self):
         """Clear all stored items in logger cache."""
         self.created_on = datetime.now(timezone.utc)
         self.metrics = []
         self.params = {}
         self.artifacts = []
+        self.db_logs = {}
